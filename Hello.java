c504a3d60d6250f35a50ea5ae493e33d8a1ef209
testdavinder <testdavinder@gmail.com>
2024-05-20 19:13:46 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+        static int x = 5;
+        
+        public static void main(String[ ] args) {
+        int x = 10;
+        System.out.println(x);
+        }
+        }  
